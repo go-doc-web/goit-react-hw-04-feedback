72a2124a24ce84b33af69d7daf5aa60760a71f32
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-02-05 23:49:12 +0200

    add app on the hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import styles from './App.module.scss';
 import Section from './Section';
@@ -6,56 +6,110 @@ import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  positivePercentage = (good, total) => {
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+
+  const positivePercentage = (good, total) => {
     if (!total) {
       return;
     }
     return +((good / total) * 100).toFixed(2);
   };
 
-  handlefeedbackBtn = name => {
-    this.setState(prevstate => ({
-      [name]: prevstate[name] + 1,
-    }));
+  const handlefeedbackBtn = name => {
+    setFeedback(prevState => {
+      const value = prevState[name];
+      return { ...feedback, [name]: value + 1 };
+    });
   };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentageFeedback = this.positivePercentage(good, total);
-    const optionsFeedback = Object.keys(this.state);
 
-    return (
-      <div className={styles.wrapper}>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={optionsFeedback}
-            onClickFeedbackBtn={this.handlefeedbackBtn}
+  const optionsFeedback = Object.keys(feedback);
+
+  return (
+    <div className={styles.wrapper}>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={optionsFeedback}
+          onClickFeedbackBtn={handlefeedbackBtn}
+        />
+      </Section>
+
+      {total ? (
+        <Section title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage(good, total)}
           />
         </Section>
-
-        {total ? (
-          <Section title={'Statistics'}>
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentageFeedback}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+// good: 0,
+// neutral: 0,
+// bad: 0,
+//   };
+
+// positivePercentage = (good, total) => {
+//   if (!total) {
+//     return;
+//   }
+//   return +((good / total) * 100).toFixed(2);
+// };
+
+//   handlefeedbackBtn = name => {
+//     this.setState(prevstate => ({
+//       [name]: prevstate[name] + 1,
+//     }));
+//   };
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+//     const positivePercentageFeedback = this.positivePercentage(good, total);
+//     const optionsFeedback = Object.keys(this.state);
+
+// return (
+//   <div className={styles.wrapper}>
+//     <Section title={'Please leave feedback'}>
+//       <FeedbackOptions
+//         options={optionsFeedback}
+//         onClickFeedbackBtn={this.handlefeedbackBtn}
+//       />
+//     </Section>
+
+//     {total ? (
+//       <Section title={'Statistics'}>
+//         <Statistics
+//           good={good}
+//           neutral={neutral}
+//           bad={bad}
+//           total={total}
+//           positivePercentage={positivePercentageFeedback}
+//         />
+//       </Section>
+//     ) : (
+//       <Notification message="There is no feedback" />
+//     )}
+//   </div>
+//     );
+//   }
+// }
+
+// export default App;
